388326e4fbc9b637109f80e5d93f6e974d46eeb8
kushparsaniya-vasyerp <kushparsaniya@vasyerp.com>
2025-01-17 16:01:52 +0530

    changes in test cases.

--- src/test/java/dev/kush/catalogservice/CatalogServiceApplicationTests.java
+++ src/test/java/dev/kush/catalogservice/CatalogServiceApplicationTests.java
@@ -4,6 +4,7 @@ import dev.kush.catalogservice.domain.Book;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
@@ -13,7 +14,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest(
         webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
 )
-@ActiveProfiles(value = "test")
+//@ActiveProfiles(value = "test")
 class CatalogServiceApplicationTests {
 
     @Autowired
@@ -21,7 +22,7 @@ class CatalogServiceApplicationTests {
 
     @Test
     void whenPostRequestThenBookCreated() {
-        var book = Book.of("1234567890", "spring start here", "laur spilca", 599d);
+        var book = Book.of("879453844328", "spring start here", "laur spilca", 599d);
         webTestClient
                 .post()
                 .uri("/books")
@@ -64,8 +65,11 @@ class CatalogServiceApplicationTests {
         webTestClient.get()
                 .uri("/books/" + isbn)
                 .exchange()
+                .expectStatus().isEqualTo(HttpStatus.OK)
                 .expectBody(Book.class)
-                .isEqualTo(book);
+                .value(savedBook ->
+                        assertThat(savedBook.isbn()).isEqualTo(book.isbn())
+                );
     }
 
     @Test
@@ -79,7 +83,8 @@ class CatalogServiceApplicationTests {
 
         webTestClient.delete()
                 .uri("/books/" + isbn)
-                .exchange();
+                .exchange()
+                .expectStatus().isEqualTo(HttpStatus.NO_CONTENT);
 
         webTestClient.get()
                 .uri("/books/" + isbn)
